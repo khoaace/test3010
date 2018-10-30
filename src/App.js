0e1cc13869ab9820e629c96bcde007eee8d34123
Tran Dang Khoa <khoatd@evolableasia.vn>
2018-10-30 23:23:12 +0700

    initial react firebase

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import firebase, { auth, provider } from "./config/firebase";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentItem: "",
+      username: "",
+      items: [],
+      user: null // <-- add this line
+    };
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      if (user) {
+        this.setState({ user });
+      }
+    });
+  }
+
+  logout = () => {
+    auth.signOut().then(() => {
+      this.setState({
+        user: null
+      });
+    });
+  };
+
+  login = () => {
+    auth.signInWithPopup(provider).then(result => {
+      const user = result.user;
+      this.setState({
+        user
+      });
+    });
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          {this.state.user ? (
+            <button
+              onClick={this.logout}
+              style={{ width: "200px", height: "200px" }}
+            >
+              Log Out
+            </button>
+          ) : (
+            <button
+              onClick={this.login}
+              style={{ width: "200px", height: "200px" }}
+            >
+              Log In
+            </button>
+          )}
         </header>
+        {this.state.user ? (
+          <div>
+            <div className="user-profile">
+              <img src={this.state.user.photoURL} />
+            </div>
+          </div>
+        ) : (
+          <div className="wrapper">
+            <p>
+              You must be logged in to see the potluck list and submit to it.
+            </p>
+          </div>
+        )}
       </div>
     );
   }
---
+++ src/config/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+const config = {
+  apiKey: "AIzaSyCkyGjw4vdSlBEDpU3q7lHI6ooAy1kjQIk",
+  authDomain: "chat-group-tdk.firebaseapp.com",
+  databaseURL: "https://chat-group-tdk.firebaseio.com",
+  projectId: "chat-group-tdk",
+  storageBucket: "chat-group-tdk.appspot.com",
+  messagingSenderId: "551455828957"
+};
+firebase.initializeApp(config);
+export const provider = new firebase.auth.GoogleAuthProvider();
+export const auth = firebase.auth();
+export default firebase;
